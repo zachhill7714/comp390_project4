3cf804e7df63a4199d16145d71b1e0936e5165fa
zachhill7714 <zachhill7714@gmail.com>
2024-02-27 14:30:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ def app():
     print("Hello world")
 
     api_key = get_api_key()[0]
-    google_api_key = get_api_key()[1]
 
     user_input = ""
     while user_input == "":
         user_input = input("Enter the zipcode you want to see the weather for: ")
 
     try:
-        location_data = get(f"https://maps.googleapis.com/maps/api/geocode/json?address={user_input}&sensor=true&key={google_api_key}", timeout=5)
+        location_data = get(f"http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey={api_key}&q={user_input}", timeout=5)
     except exceptions.Timeout:
         print("Getting data timed out, please try again")
         return
@@ -25,27 +24,14 @@ def app():
 
     json_location_data = loads(location_data.text)
 
-    if json_location_data["results"]:
-        address_components = json_location_data["results"][0]["address_components"]
-        city = address_components[1]["long_name"]
-        state = address_components[3]["long_name"]
-        print(f"Your location is: {city}, {state}")
-
-    try:
-        accuweather_location_data = get(f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={city}%2C{state}", timeout=5)
-    except exceptions.Timeout:
-        print("Getting data timed out, please try again")
-        return
-
-    if accuweather_location_data.status_code != 200:
-        print("Could not fetch the data, please try again")
-        return
-
-    json_accuweather_location_data = loads(accuweather_location_data.text)
-
     location_code = ""
-    if json_accuweather_location_data:
-        location_code = json_accuweather_location_data[0]["Key"]
+    city = ""
+    state = ""
+    if json_location_data:
+        location_code = json_location_data[0]["ParentCity"]["Key"]
+        city = json_location_data[0]["LocalizedName"]
+        state = json_location_data[0]["AdministrativeArea"]["LocalizedName"]
+        print(f"Your location is: {city}, {state}")
 
     try:
         current_conditions = get(f"http://dataservice.accuweather.com/currentconditions/v1/{location_code}?apikey={api_key}", timeout=5)
